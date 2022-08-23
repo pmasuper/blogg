2573ad6ff624332011a612a5921284b471e26440
Mahad Asif <pmasuper34@gmail.com>
2022-08-23 16:01:50 +0500

    forms and user accounts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 
@@ -9,3 +10,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
